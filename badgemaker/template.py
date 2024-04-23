283b67bd0f8ff796bc57b76f5c6280bdb23ab17e
Felipe Catapano Emrich Melo <mefkhytheroboticist@gmail.com>
2024-04-22 21:26:57 -0300

    Fixing templates

--- badgemaker/template.py
+++ badgemaker/template.py
@@ -3,7 +3,7 @@ from PIL import ImageFont, ImageDraw, Image
 import os
 
 def insert_photo(template, photo):
-    mask_green = cv2.inRange(template, (0, 250, 0), (5, 255, 5))
+    mask_green = cv2.inRange(template, (0, 180, 0), (100, 255, 100))
     contours_green, tree = cv2.findContours(mask_green, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     x, y, w, h = cv2.boundingRect(contours_green[0])
     photo = cv2.resize(photo, (w, h), interpolation=cv2.INTER_CUBIC)
@@ -18,19 +18,15 @@ def insert_text(template, text, font_path):
     mask_red = cv2.inRange(template, (0, 0, 250), (5, 5, 255))
     contours_red, tree = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     x, y, w, h = cv2.boundingRect(contours_red[0])
-    font_size = h / len(text.split(' '))  # This is a simple estimation
+    text = text.replace(' ', '\n')
+    font_size = 5 * h / len(text)  # This is a simple estimation
     font = ImageFont.truetype(font_path, font_size)
-    text_box = Image.new('RGB', (w, h), (255, 255, 255))
-    draw = ImageDraw.Draw(text_box)
-    bg_color = template[y+h//2, x+w//2]
+    bg_color = tuple(template[y+h//2, x+w//2])
     text_color = (255, 255, 255) if sum(bg_color) < 383 else (0, 0, 0)
+    text_box = Image.new('RGB', (w, h), bg_color)
+    draw = ImageDraw.Draw(text_box)
     draw.text((0, 0), text, font=font, fill=text_color)
     text_box = text_box.resize((w, h))
-    mask_red_copy = mask_red[y:y+h, x:x+w]
-    for i in range(y, y+h):
-        for j in range(x, x+w):
-            if mask_red_copy[i-y, j-x] == 255:
-                template[i, j] = bg_color
     template[y:y+h, x:x+w] = text_box
     return template
 
